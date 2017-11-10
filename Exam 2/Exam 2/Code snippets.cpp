ed57a6ccc56e4fc4d253ace7f5de3a24b686e54f
Unknown <zacharyd1@live.com>
2017-11-10 11:31:07 -0500

    Automated testing

--- Exam 2/Exam 2/Code snippets.cpp
+++ Exam 2/Exam 2/Code snippets.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 void validWeekday(int day)
 {
-	int weekday;
-	cin >> weekday;
-	if (0 < weekday < 8)
+	int weekday = day;
+	//cin >> weekday;
+	if (0 < weekday && weekday < 8)
 		cout << "Input is valid." << endl;
 	else
 		cout << "Input is invalid." << endl;
@@ -14,7 +14,11 @@ void validWeekday(int day)
 
 int main()
 {
-	validWeekday(5);
+	for (int i = -5; i < 10; i++)
+	{
+		cout << i << " | ";
+		validWeekday(i);
+	}
 
 	cin.get(); cin.get();	//Hold the window open
 
