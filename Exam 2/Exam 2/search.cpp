df57e81cb3519a0bc55057aea3551ba982f208cb
Unknown <zacharyd1@live.com>
2017-11-10 11:05:53 -0500

    Made the given code, yaknow, COMPILE AT ALL

--- Exam 2/Exam 2/search.cpp
+++ Exam 2/Exam 2/search.cpp
@@ -1,22 +1,29 @@
-  int main(){
-  
-  string wordForQuery;
-  
-  cout<<"Enter a word to be searched in the file words.txt:";
-  cin>>wordForQuery;
-  
-  ifstream inFile;
-  inFile.open("words.txt");
-  
-  string word;
-  
-  while (inFile){
-    inFile >> word;
-  
-    if (word == wordForQuery){
-        cout << "Found!"<<endl;
-    }
-  }
-  
-  return 0;
-  }
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main() {
+
+	string wordForQuery;
+
+	cout << "Enter a word to be searched in the file words.txt:";
+	cin >> wordForQuery;
+
+	ifstream inFile;
+	inFile.open("words.txt");
+
+	string word;
+
+	while (inFile) {
+		inFile >> word;
+
+		if (word == wordForQuery) {
+			cout << "Found!" << endl;
+		}
+	}
+
+	return 0;
+}
