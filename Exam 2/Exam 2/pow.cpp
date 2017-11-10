a326f218903a70e4593c3809d6c72646c90a7caa
Unknown <zacharyd1@live.com>
2017-11-10 10:51:54 -0500

    Completed Question 2

--- Exam 2/Exam 2/pow.cpp
+++ Exam 2/Exam 2/pow.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //I'm assuming that the #include <iomanip> was left out by mistake, and not meant to be the solution to this question because the rest of the headers are up at the top, but I'm putting it here just in case
 
 #include <iomanip>		//setprecision() is defined in iomanip, and therefore iomanip needs to be included in order for the program to compile
-void power(double, int, double);	//This is needed to tell the compiler that power is 
+void power(double, int, double&);	//This is needed to tell the compiler that power is 
 
 int main() {
 	double result = 0;
@@ -18,10 +18,14 @@ int main() {
 	cout << "10 to the power of 6 yields " << setprecision(0) << result << endl;
 	// Q2. The current version outputs 0 for result. Modify the definition of function power() such that the program outputs 1,000,000 rather than 0. (0.5) Explain what makes the difference. (0.5)
 
+	//Changing 'double result' to 'double & result' changes 'result' to be a reference to a value rather than another variable.  Because of this, when 'result' is changed in the funciton, it is also changing 'result' in the main function, instead of a copy of it.
+
+	cin.get();	//Holds the window open
+
 	return 0;
 }
 
-void power(double base, int exponent, double result) {
+void power(double base, int exponent, double & result) {
 	result = 1;
 	while (exponent>0) {
 		result = base * result;
