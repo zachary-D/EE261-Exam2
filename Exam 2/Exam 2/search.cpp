7074557041d50ef647e7464c1fa16c7144be25ba
Unknown <zacharyd1@live.com>
2017-11-10 11:05:16 -0500

    Copied given code

---
+++ Exam 2/Exam 2/search.cpp
@@ -0,0 +1,22 @@
+  int main(){
+  
+  string wordForQuery;
+  
+  cout<<"Enter a word to be searched in the file words.txt:";
+  cin>>wordForQuery;
+  
+  ifstream inFile;
+  inFile.open("words.txt");
+  
+  string word;
+  
+  while (inFile){
+    inFile >> word;
+  
+    if (word == wordForQuery){
+        cout << "Found!"<<endl;
+    }
+  }
+  
+  return 0;
+  }
